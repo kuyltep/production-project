0a9a43153456009df008c5cd162e7180b9afeb83
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-08-14 15:58:56 +0300

    add theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme } from "./theme/ThemeContext";
 
 const App = () => {
+  const [theme, setTheme] = useState(Theme.LIGHT);
+  const toogleTheme = () => {
+    setTheme((theme) => (theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
+  };
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={() => toogleTheme()}>Change Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={"Loading..."}>
